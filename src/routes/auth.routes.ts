b031bef64522a107c2a8dedd8dddef301e9bf8a5
Asfer Ali <asferali004@gmail.com>
2025-08-08 19:27:34 +0500

    updated delete account endpoint

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -18,6 +18,6 @@ router.post("/register", catchAsync(register));
 
 router.post("/login", catchAsync(login));
 
-router.delete("/account", catchAsync(deleteAccount));
+router.post("/account", catchAsync(deleteAccount));
 
 export default router;
